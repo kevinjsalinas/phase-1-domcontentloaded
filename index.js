dd27ac176cc1bb2bd7598d205acbedaed0b119e0
Kevin Salinas <kevinsalinas155@gmail.com>
2023-02-01 10:36:47 -0500

    completed domcontent loaded lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+//document.addEventListener("DOMContentLoaded", function(e) {
+ // console.log(e); 
+//});
+
+const paragraph = document.querySelector("p");
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    paragraph.textContent = "This is really cool!";
+
+    console.log("The DOM has loaded")
+}
+    
+);
+
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  //const p = document.querySelector('#text')
+//addEventListener("DOMContentLoaded", (event) => {
+
+  //  p.textContent = "This is really cool!"
+//})
